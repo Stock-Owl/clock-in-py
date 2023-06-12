34318ea7ad63554c81de0c30dda8db9173afafa3
Ivánczi Balázs <balazsivanczi06@gmail.com>
2023-06-12 12:48:40 +0200

    added t_sum method, w.i.p., updated initializer

--- logclass.py
+++ logclass.py
@@ -34,6 +34,8 @@ from os import getcwd as gwd
 %V	ISO 8601 weeknumber (01-53)	01
 """                                                                                            
 
+#TODO: sum func etc.
+
 class Log:
     """
     I should write a class description here... hehehe
@@ -43,9 +45,13 @@ class Log:
                  project: str = "",
                  log_type: str = "LOG",
                  state: str = "",
-                 log_path: str = "./tlog/"):
+                 log_path: str = "./tlog/",
+                 log_filename: str = ""):
 
-        """Initializes a Log object all values are set to N/A unless specified in arguments of the initializer"""
+        """
+        Initializes a Log object all values are set to 'N/A' unless
+        specified in arguments of the initializer except path and filename
+        """
 
         self.user: str = "N/A" if user == "" else user
         self.state: str = "N/A" if state == "" else state
@@ -54,23 +60,59 @@ class Log:
         self.log_type: str = log_type
         # self.log_path has a default
         self.log_path: str = log_path
+        self.log_filename: str = f"{dt.now().strftime(r'%Y-%m-%d')}.tlog" \
+            if log_filename == "" else f"{log_filename}.tlog"
+        # NOT a class attribute (because fuck you, that's why)
+        full_path: str = f"{self.log_path}/{self.log_filename}"
 
-    def s_time(time_format: str = '%Y-%d %H:%M:',
-               display_format: str = "{} — {}",
-               range_end: dt | str = dt.now()) -> str:
+        with open(full_path, mode = 'w+', encoding = "utf-8") as f:
+            pass
+    def t_sum():
+        ...
+
+    def l_sum(self, path: str = "") -> tuple[tuple[str, dt], ...]:
+        """Sums up hours logged by each user and returns a 2d tuple storing the sums and users in pairs (tuple(tuple))
+        
+        Requires a path to the *.tlog file"""
+
+        if path == "": path = f"{self.log_path}/{self.log_filename}"
+        print(path)
+        # sum_ stores the summed times of useres. Each user's time is stored independetnly
+        # (hence: tuple(tuple)), first: user second: summed time
+        sum_: tuple[tuple[str, dt], ...]
+        with open(path, mode = 'r+', encoding='utf-8') as f:
+            for line in f:
+                print(line, end="")
+                line = line.strip('[]')
+                dirs: list[str] = line.split('][')
+
+
+    def s_time(self,   
+                time_format: str = r"%Y-%%d %H:%M:",
+                display_format: str = "{} — {}",
+                range_end: dt | str = dt.now()) -> str:
+        
         if isinstance(range_end, str):
             range_end = dt(range_end)
         now: dt = dt.now()
-        now.microsecond = 0
-        range_end.microsecond = 0
-        if now != range_end:
+
+        """
+        this is the fun part
+        compares the strings, since datetime object attributes are read only
+        therefore it doesn't compare on a microsecond level
+        unless specified in time_format
+        for some dumb ass reason the 'resolution' attribute
+        is read-only too so this is what I hat to resort to
+        """
+
+        if now.strftime(time_format) != range_end.strftime(time_format):
             timerange = display_format.format(
                 now.strftime(time_format),
                 range_end.strftime(time_format))
             return timerange
         return now.strftime(time_format)
 
-    def mk_log_line(self, log_type: str = "", state: str = "", user: str = "", project: str = "", t_format: str = "%H:%M") ->  None:
+    def mk_log_line(self, log_type: str = "", state: str = "", user: str = "", project: str = "", t_format: str = r"%H:%M") ->  str:
         """generates a log line from given arguments in pre-defined format or using properties given at initialization"""
         if log_type == "": log_type = self.log_type 
         if state == "": state = self.state
@@ -78,21 +120,23 @@ class Log:
         if project == "": project = self.project
     #
         cwd_dir: str = gwd()
-        time_:str = self.s_time(format = t_format)
-        log_line: str = f"[{log_type}][{state}][{time_}][{user}]/[{project}]@[{cwd_dir}]\n"
+        time_: str = self.s_time(time_format = t_format)
+        log_line: str = f"[{log_type}][{state}][{time_}][{user}][{project}][{cwd_dir}]\n"
         return log_line
 
-    def log(self, path: str = "", log_line: str = ""):
+    def log(self, path: str = "", filename = "", log_line: str = ""):
         """writes log line into specifies .tlog file. Makes a log line if none is given"""
         if path == "":
-            path = self.log_path        #defaults to predefined path './tlog/'
+            path = self.log_path    #defaults to predefined path './tlog/'
+        if filename == "":
+            filename = self.log_filename    #defaults to predefined filename '[current date].tlog'
         if log_line == "":
             log_line = self.mk_log_line(self.log_type, self.state, self.user, self.project)
-        with open(path, mode = "a+", encoding = "utf8") as f:
-            f.write(log_line + "\n")
+        full_path = f"{path}/{filename}"
+        with open(full_path, mode = "a+", encoding = "utf8") as f:
+            f.write(log_line)
 
     def __str__(self):
         """returns a nicely formatted string version of the object's properties"""
 
         return f"—————\nLog class object\nprops:\n\t>user: {self.user}\n\t>state: {self.state}\n\t>project: {self.project}\n\t>tpye: {self.log_type}\n\t@ {gwd()}\n—————"
-
